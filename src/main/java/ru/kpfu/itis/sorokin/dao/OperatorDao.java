c7a6ef6c83c7130b909826f36578bba151d1ede8
egorX_x <sorokine329@gmail.com>
2025-10-21 12:26:08 -0300

    feat: findByUserId method in OperatorDao

--- src/main/java/ru/kpfu/itis/sorokin/dao/OperatorDao.java
+++ src/main/java/ru/kpfu/itis/sorokin/dao/OperatorDao.java
@@ -3,9 +3,12 @@ package ru.kpfu.itis.sorokin.dao;
 import ru.kpfu.itis.sorokin.entity.Operator;
 
 import java.sql.Connection;
+import java.util.Optional;
 
 public interface OperatorDao {
     Operator save(Operator operator);
 
     Operator save(Operator operator, Connection connection);
+
+    Optional<Operator> findByUserId(Integer userId);
 }
--- src/main/java/ru/kpfu/itis/sorokin/dao/impl/OperatorDaoImpl.java
+++ src/main/java/ru/kpfu/itis/sorokin/dao/impl/OperatorDaoImpl.java
@@ -2,13 +2,16 @@ package ru.kpfu.itis.sorokin.dao.impl;
 
 import ru.kpfu.itis.sorokin.dao.OperatorDao;
 import ru.kpfu.itis.sorokin.entity.Operator;
+import ru.kpfu.itis.sorokin.entity.Status;
 import ru.kpfu.itis.sorokin.exception.DataAccessException;
 import ru.kpfu.itis.sorokin.util.DataBaseConnectionUtil;
 
 import java.sql.*;
+import java.util.Optional;
 
 public class OperatorDaoImpl implements OperatorDao {
     private static final String SQL_SAVE = "INSERT INTO operator (user_id, company_name, description, status) VALUES (?, ?, ?, ?::general_status)";
+    private static final String SQL_FIND_BY_USER_ID = "SELECT user_id, company_name, description, status FROM operator WHERE user_id = ?";
 
     @Override
     public Operator save(Operator operator) {
@@ -38,5 +41,34 @@ public class OperatorDaoImpl implements OperatorDao {
         } catch (SQLException e) {
             throw new DataAccessException("Failed save operator", e);
         }
+    }
+
+    @Override
+    public Optional<Operator> findByUserId(Integer userId) {
+        try (Connection connection = DataBaseConnectionUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_BY_USER_ID)) {
+
+            preparedStatement.setInt(1, userId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return Optional.of(
+                            new Operator(
+                                    userId,
+                                    resultSet.getString("company_name"),
+                                    resultSet.getString("description"),
+                                    Status.valueOf(resultSet.getString("status"))
+                            )
+                    );
+                }
+            } catch (SQLException e) {
+                throw new DataAccessException("Failed select operator by user_id");
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed select operator by user_id");
+        }
+
+        return Optional.empty();
     }
 }
