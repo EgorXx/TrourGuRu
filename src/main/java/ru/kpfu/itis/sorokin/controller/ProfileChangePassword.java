61b1a94c18645d2f38ab6ff49936d5b54d3224f8
egorX_x <sorokine329@gmail.com>
2025-10-29 21:37:59 +0300

    feat: Implement password change functionality

---
+++ src/main/java/ru/kpfu/itis/sorokin/controller/ProfileChangePassword.java
@@ -0,0 +1,96 @@
+package ru.kpfu.itis.sorokin.controller;
+
+import ru.kpfu.itis.sorokin.dto.UserSessionDto;
+import ru.kpfu.itis.sorokin.exception.ValidationException;
+import ru.kpfu.itis.sorokin.service.OperatorService;
+import ru.kpfu.itis.sorokin.service.UserService;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/profile/change-password")
+public class ProfileChangePassword extends HttpServlet {
+    private UserService userService;
+    private OperatorService operatorService;
+
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+
+        userService = (UserService) servletContext.getAttribute("userService");
+        operatorService = (OperatorService) servletContext.getAttribute("operatorService");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+
+        if (session == null) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+
+        UserSessionDto userSessionDto = (UserSessionDto) session.getAttribute("user");
+
+        if (userSessionDto == null) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+
+        String currentPassword = req.getParameter("currentPassword");
+        String newPasswrod = req.getParameter("newPassword");
+        String confirmPassword = req.getParameter("confirmPassword");
+
+        Map<String, String> errors = new HashMap<>();
+
+        errors = validateChangePassword(currentPassword, newPasswrod, confirmPassword);
+
+        if (!errors.isEmpty()) {
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher("/profile_edit.ftl").forward(req, resp);
+            return;
+        }
+
+        try {
+            userService.changePassword(userSessionDto.id(), currentPassword, newPasswrod);
+
+            resp.sendRedirect(req.getContextPath() + "/profile/edit?success=true");
+
+        } catch (ValidationException e) {
+            req.setAttribute("errors", e.getErrors());
+            req.getRequestDispatcher("/profile_edit.ftl").forward(req, resp);
+        }
+
+    }
+
+    private Map<String, String> validateChangePassword(String currentPassword, String newPasswrod, String confirmPassword) {
+        Map<String, String> errors = new HashMap<>();
+
+        if (currentPassword == null || currentPassword.isBlank()) {
+            errors.put("currentPassword", "Поле \"Текущий пароль\" не может быть пустым");
+        }
+
+        if (newPasswrod == null || newPasswrod.isBlank()) {
+            errors.put("newPasswrod", "Поле \"Новый пароль\" не может быть пустым");
+        }
+
+        if (confirmPassword == null || confirmPassword.isBlank()) {
+            errors.put("confirmPassword", "Поле \"Подтверждение пароля\" не может быть пустым");
+        }
+
+        if (!newPasswrod.equals(confirmPassword)) {
+            errors.put("errorPassword", "Введенные пароли не совпадают");
+        }
+
+        return errors;
+    }
+}
