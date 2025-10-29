7786ab4ec8cf0d85c67d0835f622ae492ac91413
egorX_x <sorokine329@gmail.com>
2025-10-29 23:02:03 +0300

    feat: entities - ApplicationTour and AddApplicationTourDto, for add application tour

---
+++ src/main/java/ru/kpfu/itis/sorokin/dto/AddApplicationTourDto.java
@@ -0,0 +1,3 @@
+package ru.kpfu.itis.sorokin.dto;
+
+public record AddApplicationTourDto(Integer userId, Integer tourId) {}
---
+++ src/main/java/ru/kpfu/itis/sorokin/entity/ApplicationTour.java
@@ -0,0 +1,49 @@
+package ru.kpfu.itis.sorokin.entity;
+
+public class ApplicationTour {
+    private Integer id;
+    private Integer userId;
+    private Integer tourId;
+    private Status status;
+
+    public ApplicationTour(Integer id, Integer userId, Integer tourId, Status status) {
+        this.id = id;
+        this.userId = userId;
+        this.tourId = tourId;
+        this.status = status;
+    }
+
+    public ApplicationTour() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getTourId() {
+        return tourId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setTourId(Integer tourId) {
+        this.tourId = tourId;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
