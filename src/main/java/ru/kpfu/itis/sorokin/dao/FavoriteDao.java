0748c78f9e7c63c663e5c3f4ab3971dc5a594a71
egorX_x <sorokine329@gmail.com>
2025-11-02 02:04:36 +0300

    feat: findAllFavoritesByUserId method in FavoriteDao

--- src/main/java/ru/kpfu/itis/sorokin/dao/FavoriteDao.java
+++ src/main/java/ru/kpfu/itis/sorokin/dao/FavoriteDao.java
@@ -1,7 +1,10 @@
 package ru.kpfu.itis.sorokin.dao;
 
+import java.util.List;
+
 public interface FavoriteDao {
     void addFavorite(Integer userId, Integer tourId);
     void removeFavorite(Integer userId, Integer tourId);
     boolean isFavorite(Integer userId, Integer tourId);
+    List<Integer> findAllFavoritesByUserId(Integer userId);
 }
--- src/main/java/ru/kpfu/itis/sorokin/dao/impl/FavoriteDaoImpl.java
+++ src/main/java/ru/kpfu/itis/sorokin/dao/impl/FavoriteDaoImpl.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavoriteDaoImpl implements FavoriteDao {
     private static final String SQL_SAVE = "INSERT INTO favorite (user_id, tour_id) VALUES (?, ?)";
@@ -15,6 +17,8 @@ public class FavoriteDaoImpl implements FavoriteDao {
     private static final String SQL_DELETE = "DELETE FROM favorite WHERE user_id = ? AND tour_id = ?";
 
     private static final String SQL_EXISTS = "SELECT EXISTS(SELECT 1 FROM favorite WHERE user_id = ? AND tour_id = ?)";
+
+    private static final String SQL_FIND_ALL_BY_USER_ID = "SELECT tour_id FROM favorite WHERE user_id = ?";
 
     @Override
     public void addFavorite(Integer userId, Integer tourId) {
@@ -66,6 +70,28 @@ public class FavoriteDaoImpl implements FavoriteDao {
 
         } catch (SQLException e) {
             throw new DataAccessException("Failed check exists favorite", e);
+        }
+    }
+
+    @Override
+    public List<Integer> findAllFavoritesByUserId(Integer userId) {
+        try (Connection connection = DataBaseConnectionUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_ALL_BY_USER_ID)) {
+
+            preparedStatement.setInt(1, userId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                List<Integer> tourIds = new ArrayList<>();
+
+                while(resultSet.next()) {
+                    tourIds.add(resultSet.getInt("tour_id"));
+                }
+
+                return tourIds;
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed select tour ids favorite by user id", e);
         }
     }
 }
